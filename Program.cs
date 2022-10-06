51e41acd59f03075d962044c0413f18f3dda8759
szafranic <ryan.szafranic@gmail.com>
2022-10-06 09:14:16 -0400

    big push

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Business
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("We are about to have a business ");
+
+			Console.WriteLine("This is our main code");
+
+			Console.WriteLine("This is our footer");
+
+		}
+	}
+}
